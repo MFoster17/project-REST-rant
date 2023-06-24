95881d1779065051b57b4736db5c6634b1f2cd9f
MFoster17 <ausslanderofficial@gmail.com>
2023-06-24 05:32:40 -0700

    Show route connected

--- Controllers/places.js
+++ Controllers/places.js
@@ -25,7 +25,20 @@ app.get('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
-  
+
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id] })
+    }
+  })
+
   function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
